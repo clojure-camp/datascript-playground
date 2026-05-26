c0548630381eb5d526d3972d4b65fadc32accdeb
Rafal Dittwald <rafal.dittwald@gmail.com>
2026-05-26 12:03:11 -0400

    Add comment re: reloading examples

--- src/playground/examples.cljs
+++ src/playground/examples.cljs
@@ -2,4 +2,6 @@
   (:require-macros
    [playground.examples-macro :refer [load-example-sets]]))
 
+;; Must "touch" this file so that shadow-cljs picks up changes in the examples
+
 (def sets (load-example-sets))
